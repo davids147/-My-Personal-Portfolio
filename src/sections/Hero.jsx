028a0324bb30af8dbd975ac2c57f3bfcadc70d5d
david <davidroyalz471.com>
2026-09-08 22:33:10 +0530

    Update hero section and add resume

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -95,10 +95,12 @@ export const Hero = () => {
               <Button size="lg">
                 Contact Me <ArrowRight className="w-5 h-5" />
               </Button>
+              <a href="public/David_sarella.pdf" target="_blank" rel="noopener noreferrer">
               <AnimatedBorderButton>
                 <Download className="w-5 h-5" />
                 Download CV
               </AnimatedBorderButton>
+              </a>
             </div>
 
             {/* Social Links */}
