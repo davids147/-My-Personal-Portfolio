0de20c1cc0a00996b1002be0cbeae7d0059569ff
davids147 <davidroyalz471@gmail.com>
2026-09-09 13:34:45 +0530

    Update avatar image for David Sarella

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -8,7 +8,7 @@ const testimonials = [
     author: "David Sarella",
     role: "CEO, Innovation Hub",
     avatar:
-      "/IMG_5465.JPG",
+      "/IMG_5465-removebg-preview.png,
   },
     {
     quote:
